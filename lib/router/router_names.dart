351a60d1c1817b15ffedb16dbb397410f41897e0
1Shibam <hehe.shibam@gmail.com>
2025-02-26 19:43:42 +0530

    update router names

--- lib/router/router_names.dart
+++ lib/router/router_names.dart
@@ -1,4 +1,6 @@
 class RouterNames {
   static const String splash = '/';
   static const String home = '/home';
+  static const String login = '/login';
+  static const String signup = '/signup';
 }
