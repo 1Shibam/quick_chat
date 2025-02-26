f3900b1666ecb8de110f91c0d2828e908ee170b4
1Shibam <hehe.shibam@gmail.com>
2025-02-26 17:48:53 +0530

    minor setup fixes

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -3,47 +3,45 @@ import 'package:go_router/go_router.dart';
 import 'package:quick_chat/screens/home_screen.dart';
 import 'package:quick_chat/screens/splash_screen.dart';
 
-GoRouter router = GoRouter(initialLocation: '/',routes: [
-  splashScreenRoute(),
-]);
+GoRouter router = GoRouter(
+    initialLocation: '/', routes: [splashScreenRoute(), homeScreenRoute()]);
 
 GoRoute splashScreenRoute() {
   return GoRoute(
-path: '/',
-pageBuilder: (context, state) {
-  return CustomTransitionPage(
-    key: state.pageKey,
-    child: const SplashScreen(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      // Change the opacity of the screen using a Curve based on the the animation's
-      // value
-      return FadeTransition(
-        opacity:
-            CurveTween(curve: Curves.easeInOutCirc).animate(animation),
-        child: child,
+    path: '/',
+    pageBuilder: (context, state) {
+      return CustomTransitionPage(
+        key: state.pageKey,
+        child: const SplashScreen(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          // Change the opacity of the screen using a Curve based on the the animation's
+          // value
+          return FadeTransition(
+            opacity: CurveTween(curve: Curves.easeInOutCirc).animate(animation),
+            child: child,
+          );
+        },
       );
     },
   );
-},
-);
 }
+
 GoRoute homeScreenRoute() {
   return GoRoute(
-path: '/home',
-pageBuilder: (context, state) {
-  return CustomTransitionPage(
-    key: state.pageKey,
-    child: const HomeScreen(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      // Change the opacity of the screen using a Curve based on the the animation's
-      // value
-      return FadeTransition(
-        opacity:
-            CurveTween(curve: Curves.easeInOutCirc).animate(animation),
-        child: child,
+    path: '/home',
+    pageBuilder: (context, state) {
+      return CustomTransitionPage(
+        key: state.pageKey,
+        child: const HomeScreen(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          // Change the opacity of the screen using a Curve based on the the animation's
+          // value
+          return FadeTransition(
+            opacity: CurveTween(curve: Curves.easeInOutCirc).animate(animation),
+            child: child,
+          );
+        },
       );
     },
   );
-},
-);
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,12 +1,22 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:quick_chat/router/router_names.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('LOADING'),),
+    Future.delayed(const Duration(seconds: 2), () {
+      if (context.mounted) context.go(RouterNames.home);
+    });
+
+    return Scaffold(
+      body: Center(
+        child: AnimatedTextKit(
+            animatedTexts: [FlickerAnimatedText(' LOADING ...')]),
+      ),
     );
   }
 }
