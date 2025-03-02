5e0deaa2c0170a33eb0d33ace4ebf28f41cab1a8
1Shibam <hehe.shibam@gmail.com>
2025-03-02 14:14:31 +0530

    code clearn up set up exports

---
+++ lib/Exports/common_exports.dart
@@ -0,0 +1,8 @@
+//? these are the common exports almost every file has them --
+//! don't change them
+export 'package:flutter/material.dart';
+export 'package:go_router/go_router.dart';
+export 'package:quick_chat/theme/app_colors.dart';
+export 'package:quick_chat/theme/text_styles.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:quick_chat/theme/fonts.dart';
---
+++ lib/Exports/widgets_export.dart
@@ -0,0 +1,7 @@
+//? these are the widgets exported that are used across the app 
+
+export '../widgets/common_widgets/build_alert_dialog.dart';
+export '../widgets/common_widgets/build_primary_button.dart';
+export '../widgets/common_widgets/build_snackbar.dart';
+export '../widgets/common_widgets/build_text_field.dart';
+export '../widgets/common_widgets/lottie_loading_animation.dart';
