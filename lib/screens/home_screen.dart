716e3fede8a0e1d22db61d0f386cb32e5f0988af
1Shibam <hehe.shibam@gmail.com>
2025-03-02 16:02:55 +0530

    log out feature done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:quick_chat/Exports/common_exports.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,8 +11,37 @@ class HomeScreen extends StatelessWidget {
         appBar: PreferredSize(
             preferredSize: Size.fromHeight(60.h),
             child: AppBar(
-              title: const Text('QuickChat'),
-            )),
+                title: const Text('QuickChat'),
+                leading: Builder(
+                  builder: (context) {
+                    return IconButton(
+                        onPressed: () => Scaffold.of(context).openDrawer(),
+                        icon: const Icon(Icons.menu));
+                  },
+                ))),
+        drawer: Drawer(
+          backgroundColor: AppColors.darkGreen,
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                      'Menu',
+                      style: AppTextStyles.heading1,
+                    )),
+              ),
+              ListTile(
+                title: IconButton(
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                      context.go(RouterNames.splash);
+                    },
+                    icon: const Icon(Icons.logout)),
+              )
+            ],
+          ),
+        ),
         floatingActionButton: Padding(
           padding: EdgeInsets.all(8.w),
           child: FloatingActionButton(
