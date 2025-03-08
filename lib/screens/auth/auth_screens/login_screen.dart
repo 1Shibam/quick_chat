4ff8cab29acc31c6d6524e1cb0c5c21654234eb1
1Shibam <hehe.shibam@gmail.com>
2025-03-08 21:32:34 +0530

    add the reset password functionality

--- lib/screens/auth/auth_screens/login_screen.dart
+++ lib/screens/auth/auth_screens/login_screen.dart
@@ -6,6 +6,7 @@ import 'package:quick_chat/Exports/widgets_export.dart';
 import 'package:quick_chat/preferences/login_page_preference.dart';
 
 import 'package:quick_chat/screens/auth/widgets/bottom_rich_texts_widget.dart';
+import 'package:quick_chat/screens/auth/widgets/reset_password_dialog.dart';
 import 'package:quick_chat/services/firebase_auth_service.dart';
 
 class LoginPage extends StatefulWidget {
@@ -67,111 +68,129 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.w),
         child: Center(
           child: Form(
             key: formkey, // Attach form key
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Login to continue",
-                  style: AppTextStyles.heading1,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 32.h),
-                BuildTextField(
-                  controller: emailController,
-                  focusNode: emailFocus,
-                  preWidget: const Icon(Icons.email),
-                  label: 'Email',
-                  submitField: (_) {
-                    if (emailController.text.trim().isNotEmpty) {
-                      FocusScope.of(context).requestFocus(passFocus);
-                    }
-                  },
-                  validator: validateEmail, // Pass function reference
-                ),
-                SizedBox(height: 16.h),
-                BuildTextField(
-                  controller: passController,
-                  focusNode: passFocus,
-                  label: 'Password',
-                  isPassword: isVisible,
-                  preWidget: const Icon(Icons.lock),
-                  suffWidget: IconButton(
-                    onPressed: () => setState(() {
-                      isVisible = !isVisible;
-                    }),
-                    icon: Icon(
-                        isVisible ? Icons.visibility_off : Icons.visibility),
-                    color:
-                        isVisible ? AppColors.darkGreen : AppColors.softWhite,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Login to continue",
+                    style: AppTextStyles.heading1,
+                    textAlign: TextAlign.center,
                   ),
-                  validator: validatePassword, // Pass function reference
-                  submitField: (_) => submitForm(),
-                ),
-                SizedBox(height: 24.h),
-                GestureDetector(
-                  onTap: submitForm,
-                  child: const BuildPrimaryButton(
-                    text: 'Login',
-                    // Ensure button calls submitForm()
+                  SizedBox(height: 32.h),
+                  BuildTextField(
+                    controller: emailController,
+                    focusNode: emailFocus,
+                    preWidget: const Icon(Icons.email),
+                    label: 'Email',
+                    submitField: (_) {
+                      if (emailController.text.trim().isNotEmpty) {
+                        FocusScope.of(context).requestFocus(passFocus);
+                      }
+                    },
+                    validator: validateEmail, // Pass function reference
                   ),
-                ),
-                SizedBox(height: 16.h),
-                const BottomRichTextWidget(
-                    text1: 'Don\'t have an account?',
-                    text2: ' Sign Up',
-                    pageName: RouterNames.signup),
-                SizedBox(
-                  height: 12.h,
-                ),
-                const Divider(),
-                SizedBox(
-                  height: 12.h,
-                ),
-                GestureDetector(
-                  onTap: () async {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return const LoadingAnimation(
-                          opacity: 0.4,
-                          height: 40,
-                          width: 40,
-                        );
-                      },
-                    );
-                    await FirebaseAuthServices().sigupWithGoogle(context);
-                    context.pop();
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: AppColors.darkGreen,
-                        borderRadius: BorderRadius.circular(20.r)),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        SvgPicture.asset(
-                          'assets/images/google-color-svgrepo-com.svg',
-                          height: 28.h,
-                          width: 28.w,
-                        ),
-                        Text(
-                          'Continue With Google',
-                          style: AppTextStyles.heading3,
-                        )
-                      ],
+                  SizedBox(height: 16.h),
+                  BuildTextField(
+                    controller: passController,
+                    focusNode: passFocus,
+                    label: 'Password',
+                    isPassword: isVisible,
+                    preWidget: const Icon(Icons.lock),
+                    suffWidget: IconButton(
+                      onPressed: () => setState(() {
+                        isVisible = !isVisible;
+                      }),
+                      icon: Icon(
+                          isVisible ? Icons.visibility_off : Icons.visibility),
+                      color:
+                          isVisible ? AppColors.darkGreen : AppColors.softWhite,
+                    ),
+                    validator: validatePassword, // Pass function reference
+                    submitField: (_) => submitForm(),
+                  ),
+                  SizedBox(height: 24.h),
+                  GestureDetector(
+                    onTap: submitForm,
+                    child: const BuildPrimaryButton(
+                      text: 'Login',
+                      // Ensure button calls submitForm()
                     ),
                   ),
-                ),
-                
-              ],
+                  SizedBox(height: 16.h),
+                  GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const ResetPasswordDialog();
+                        },
+                      );
+                    },
+                    child: const BuildPrimaryButton(
+                      color: Colors.transparent,
+                      text: 'Forgot Password?',
+                      // Ensure button calls submitForm()
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+                  const BottomRichTextWidget(
+                      text1: 'Don\'t have an account?',
+                      text2: ' Sign Up',
+                      pageName: RouterNames.signup),
+                  SizedBox(
+                    height: 12.h,
+                  ),
+                  const Divider(),
+                  SizedBox(
+                    height: 12.h,
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const LoadingAnimation(
+                            opacity: 0.4,
+                            height: 40,
+                            width: 40,
+                          );
+                        },
+                      );
+                      await FirebaseAuthServices().sigupWithGoogle(context);
+                      context.pop();
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: AppColors.darkGreen,
+                          borderRadius: BorderRadius.circular(20.r)),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 16.w, vertical: 12.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/google-color-svgrepo-com.svg',
+                            height: 28.h,
+                            width: 28.w,
+                          ),
+                          Text(
+                            'Continue With Google',
+                            style: AppTextStyles.heading3,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
