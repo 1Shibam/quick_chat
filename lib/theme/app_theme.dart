7bc6514b6ec45bd792577dd5cdf89eb5285c11b2
1Shibam <hehe.shibam@gmail.com>
2025-03-08 21:33:02 +0530

    update the app theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,37 +1,37 @@
-
 import 'package:quick_chat/Exports/common_exports.dart';
 
 ThemeData quickChatTheme() {
-    return ThemeData(
-            floatingActionButtonTheme: FloatingActionButtonThemeData(
-              splashColor: AppColors.grey,
-              backgroundColor: AppColors.darkGreen,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40.r)),
-            ),
-            scaffoldBackgroundColor: AppColors.greyBlack,
-            appBarTheme: AppBarTheme(
-                color: Colors.transparent,
-                centerTitle: false,
-                titleTextStyle: AppTextStyles.heading1),
-            inputDecorationTheme: InputDecorationTheme(
-                errorStyle: AppTextStyles.bodyText
-                    .copyWith(color: AppColors.errorRedAccent),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.r),
-                  borderSide: const BorderSide(color: AppColors.darkGreen),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.r),
-                  borderSide: const BorderSide(color: AppColors.darkGreen),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.r),
-                  borderSide: const BorderSide(color: AppColors.softWhite),
-                ),
-                errorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.r),
-                  borderSide: const BorderSide(color: AppColors.errorRed),
-                )),
-          );
-  }
+  return ThemeData(
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      splashColor: AppColors.grey,
+      backgroundColor: AppColors.darkGreen,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.r)),
+    ),
+    scaffoldBackgroundColor: AppColors.greyBlack,
+    appBarTheme: AppBarTheme(
+        color: Colors.transparent,
+        centerTitle: false,
+        titleTextStyle: AppTextStyles.heading1),
+    inputDecorationTheme: InputDecorationTheme(
+        errorStyle:
+            AppTextStyles.bodyText.copyWith(color: AppColors.errorRedAccent),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.r),
+          borderSide:
+              const BorderSide(color: AppColors.darkGreenAccent, width: 2),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.r),
+          borderSide:
+              const BorderSide(color: AppColors.darkGreenAccent, width: 2),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.r),
+          borderSide: const BorderSide(color: AppColors.softWhite, width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.r),
+          borderSide: const BorderSide(color: AppColors.errorRed),
+        )),
+  );
+}
