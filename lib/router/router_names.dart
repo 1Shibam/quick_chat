a64b902ccba544b2cc597b6ce8fdf92980dc981c
1Shibam <hehe.shibam@gmail.com>
2025-03-11 11:43:22 +0530

    add profile page name

--- lib/router/router_names.dart
+++ lib/router/router_names.dart
@@ -3,4 +3,5 @@ class RouterNames {
   static const String home = '/home';
   static const String login = '/login';
   static const String signup = '/signup';
+  static const String profile = '/profile';
 }
