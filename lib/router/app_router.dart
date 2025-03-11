7ea57ead02d059f643e1af1a9204b346e2a44705
1Shibam <hehe.shibam@gmail.com>
2025-03-11 11:43:13 +0530

    update the router

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -3,13 +3,15 @@ import 'package:go_router/go_router.dart';
 import 'package:quick_chat/screens/auth/auth_screens/login_screen.dart';
 import 'package:quick_chat/screens/auth/auth_screens/sign_up_screen.dart';
 import 'package:quick_chat/screens/home/home_screen.dart';
+import 'package:quick_chat/screens/home/profile_page.dart';
 import 'package:quick_chat/screens/home/splash_screen.dart';
 
 GoRouter router = GoRouter(initialLocation: '/', routes: [
   splashScreenRoute(),
   homeScreenRoute(),
   loginScreenRoute(),
-  signupScreenRoute()
+  signupScreenRoute(),
+  profilePageRoute()
 ]);
 
 GoRoute splashScreenRoute() {
@@ -19,6 +21,26 @@ GoRoute splashScreenRoute() {
       return CustomTransitionPage(
         key: state.pageKey,
         child: const SplashScreen(),
+        transitionDuration: const Duration(milliseconds: 500),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          // Change the opacity of the screen using a Curve based on the the animation's
+          // value
+          return FadeTransition(
+            opacity: CurveTween(curve: Curves.easeInOutCirc).animate(animation),
+            child: child,
+          );
+        },
+      );
+    },
+  );
+}
+GoRoute profilePageRoute() {
+  return GoRoute(
+    path: '/profile',
+    pageBuilder: (context, state) {
+      return CustomTransitionPage(
+        key: state.pageKey,
+        child: const ProfilePage(),
         transitionDuration: const Duration(milliseconds: 500),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           // Change the opacity of the screen using a Curve based on the the animation's
