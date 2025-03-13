1f0ed84109e096bde3f74064ff4efe2521872f91
1Shibam <hehe.shibam@gmail.com>
2025-03-13 12:57:34 +0530

    delete not useful

--- lib/providers/account_list_preference_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-class AccountListPreferenceNotifier extends StateNotifier<List<String>> {
-  AccountListPreferenceNotifier() : super([]);
-
-  Future<void> saveAccountList() async {}
-}
