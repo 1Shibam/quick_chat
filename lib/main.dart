228c28f63e140dbe6f66ac4a1ca65fb6e6219c8c
1Shibam <hehe.shibam@gmail.com>
2025-03-13 09:35:09 +0530

    small update

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class QuickChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
+      designSize: const Size(375, 812),
       builder: (context, child) {
         return MaterialApp.router(
             debugShowCheckedModeBanner: false,
