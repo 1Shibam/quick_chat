28615e3c42183f1e64bc85400d4ab7741c5a2db9
1Shibam <hehe.shibam@gmail.com>
2025-03-14 15:38:32 +0530

    separate current user and all user provider

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,13 +1,19 @@
 // Stream provider to fetch user
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quick_chat/Exports/common_exports.dart';
 import 'package:quick_chat/model/user_model.dart';
 
-final userProvider = StreamProvider<List<ChatUserModel>>((ref) {
+//? all user provider except current user
+final otherUserProvider = StreamProvider<List<ChatUserModel>>((ref) {
   try {
-    final snapshots =
-        FirebaseFirestore.instance.collection('chatUsers').snapshots();
+    final FirebaseAuth user = FirebaseAuth.instance;
+
+    final snapshots = FirebaseFirestore.instance
+        .collection('chatUsers')
+        .where('user_id', isNotEqualTo: user.currentUser!.uid)
+        .snapshots();
 
     final users = snapshots.map((snap) {
       return snap.docs
@@ -18,6 +24,28 @@ final userProvider = StreamProvider<List<ChatUserModel>>((ref) {
     return users;
   } on FirebaseException catch (error, stackTrace) {
     debugPrint(error.message);
+    debugPrintStack(stackTrace: stackTrace);
+    rethrow;
+  } catch (e, stackTrace) {
+    debugPrint("Unexpected Error: $e");
+    debugPrintStack(stackTrace: stackTrace);
+    rethrow;
+  }
+});
+
+//? current user provider
+final currentUserProvider = StreamProvider<ChatUserModel>((ref) {
+  try {
+    final FirebaseAuth user = FirebaseAuth.instance;
+    final String currentUseId = user.currentUser!.uid;
+
+    return FirebaseFirestore.instance
+        .collection('chatUsers')
+        .doc(currentUseId)
+        .snapshots()
+        .map((doc) => ChatUserModel.fromJson(doc.data()!));
+  } on FirebaseException catch (e, stackTrace) {
+    debugPrint(e.message);
     debugPrintStack(stackTrace: stackTrace);
     rethrow;
   } catch (e, stackTrace) {
