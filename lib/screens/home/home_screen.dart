c9bda752fc5d99ee08ab62774f2789dd12a65bb0
1Shibam <hehe.shibam@gmail.com>
2025-03-15 13:22:13 +0530

    update home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quick_chat/Exports/common_exports.dart';
 import 'package:quick_chat/providers/user_provider.dart';
@@ -6,88 +5,70 @@ import 'package:quick_chat/screens/home/widget/home_screen_widgets/drawer_widget
 import 'package:quick_chat/screens/home/widget/home_screen_widgets/floating_action_button.dart';
 import 'package:quick_chat/screens/home/widget/home_screen_widgets/home_screen_appbar.dart';
 import 'package:quick_chat/screens/home/widget/home_screen_widgets/home_screen_searchbar.dart';
+import 'package:quick_chat/screens/home/widget/home_screen_widgets/users_list.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          appBar: PreferredSize(
-              preferredSize: Size.fromHeight(60.h),
-              child: const HomeScreenAppBar()),
-          drawer: const DrawerWidgetHomeScreen(),
-          floatingActionButton: const FloadingActionButton(),
-          body: Column(
-            children: [
-              const HomeScreenSearchBar(),
-              SizedBox(
-                height: 12.h,
-              ),
-              Expanded(
-                child: Scrollbar(child: Consumer(
-                  builder: (context, ref, child) {
-                    final user = ref.watch(otherUserProvider);
-                    final searchQuery = ref.watch(searchQueryProvider);
-                    return user.when(data: (userData) {
-                      if (userData.isEmpty) {
-                        return const Center(
-                          child: Text('no users available'),
-                        );
-                      }
-                      final filteredSearch = userData.where((user) {
-                        return user.username
-                            .toLowerCase()
-                            .contains(searchQuery.toLowerCase());
-                      }).toList();
-                      if (filteredSearch.isEmpty) {
-                        return Center(
-                          child: Text(
-                            'No matching user found',
-                            style: AppTextStyles.heading2,
-                          ),
-                        );
-                      }
-                      return ListView.builder(
-                        itemCount: filteredSearch.length,
-                        itemBuilder: (context, index) {
-                          final singleUser = filteredSearch[index];
-
-                          return ListTile(
-                            leading: CircleAvatar(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10.r),
-                                child: CachedNetworkImage(
-                                  imageUrl: singleUser.profileUrl == ''
-                                      ? 'https://i.imgur.com/PcvwDlW.png'
-                                      : singleUser.profileUrl,
-                                  placeholder: (context, url) =>
-                                      const CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) =>
-                                      const Icon(Icons.person),
-                                ),
-                              ),
-                            ),
-                            title: Text(
-                              singleUser.username,
-                              style: AppTextStyles.heading3,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: SafeArea(
+        child: Scaffold(
+            appBar: PreferredSize(
+                preferredSize: Size.fromHeight(60.h),
+                child: const HomeScreenAppBar()),
+            drawer: const DrawerWidgetHomeScreen(),
+            floatingActionButton: const FloadingActionButton(),
+            body: Column(
+              children: [
+                const HomeScreenSearchBar(),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Expanded(
+                  child: Scrollbar(child: Consumer(
+                    builder: (context, ref, child) {
+                      final user = ref.watch(otherUserProvider);
+                      final searchQuery = ref.watch(searchQueryProvider);
+                      return user.when(data: (userData) {
+                        if (userData.isEmpty) {
+                          return const Center(
+                            child: Text('no users available'),
+                          );
+                        }
+                        final filteredSearch = userData.where((user) {
+                          return user.username
+                              .toLowerCase()
+                              .contains(searchQuery.toLowerCase());
+                        }).toList();
+                        if (filteredSearch.isEmpty) {
+                          return Center(
+                            child: Text(
+                              'No matching user found',
+                              style: AppTextStyles.heading2,
                             ),
                           );
-                        },
-                      );
-                    }, error: (error, stackTrace) {
-                      debugPrint('Firestore Stream Error: $error');
-                      debugPrintStack(stackTrace: stackTrace);
-                      return const Text('Something went wrong');
-                    }, loading: () {
-                      return const Center(child: CircularProgressIndicator());
-                    });
-                  },
-                )),
-              ),
-            ],
-          )),
+                        }
+                        return UsersList(filteredSearch: filteredSearch);
+                      },
+                          //errro state if stream got some error --
+                          error: (error, stackTrace) {
+                        debugPrint('Firestore Stream Error: $error');
+                        debugPrintStack(stackTrace: stackTrace);
+                        return const Text('Something went wrong');
+                      },
+                          //loading state widget
+                          loading: () {
+                        return const Center(child: CircularProgressIndicator());
+                      });
+                    },
+                  )),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
