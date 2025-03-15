56be1873b1f67bbaa5d2e579a3996e535a63da27
1Shibam <hehe.shibam@gmail.com>
2025-03-15 13:52:18 +0530

    usred modal bottom sheet instead

---
+++ lib/screens/home/widget/profile_page_widgets/image_source_selection.dart
@@ -0,0 +1,98 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:quick_chat/Exports/common_exports.dart';
+import 'package:quick_chat/model/user_model.dart';
+import 'package:quick_chat/providers/firestore_service_provider.dart';
+
+class ImageSourceOptions extends StatelessWidget {
+  const ImageSourceOptions({super.key, required this.userData});
+  final ChatUserModel userData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        return Container(
+          height: 200.h,
+          decoration: BoxDecoration(
+              color: AppColors.darkGreen,
+              borderRadius: BorderRadius.circular(20.r)),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Choose Source',
+                  style: AppTextStyles.heading1,
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () async {
+                            await ref
+                                .read(firestoreServiceStateNotifierProvider
+                                    .notifier)
+                                .changeProfilePicture(context, userData.userID,
+                                    ImageSource.camera);
+                            if (context.mounted) {
+                              Navigator.pop(context);
+                            }
+                          },
+                          icon: Icon(
+                            Icons.camera_alt_rounded,
+                            size: 40.sp,
+                            color: AppColors.softGrey,
+                          ),
+                        ),
+                        Text(
+                          'Camera',
+                          style: AppTextStyles.heading2,
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () async {
+                            await ref
+                                .read(firestoreServiceStateNotifierProvider
+                                    .notifier)
+                                .changeProfilePicture(context, userData.userID,
+                                    ImageSource.gallery);
+                            if (context.mounted) {
+                              Navigator.pop(context);
+                            }
+                          },
+                          icon: Icon(
+                            Icons.image,
+                            size: 40.sp,
+                            color: AppColors.softGrey,
+                          ),
+                        ),
+                        Text(
+                          'Gallery',
+                          style: AppTextStyles.heading2,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
