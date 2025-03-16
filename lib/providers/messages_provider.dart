c4d94af097f546fdb7aa3f59f613eaf2d91091ab
1Shibam <hehe.shibam@gmail.com>
2025-03-16 15:24:58 +0530

    adds message stream provider

--- lib/providers/messages_provider.dart
+++ lib/providers/messages_provider.dart
@@ -1,3 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quick_chat/Exports/common_exports.dart';
 
-final messageProvider = StreamProvider((ref) {});
+
+final messageProvider = StreamProvider((ref) {
+  try {
+    final snapshots =
+        FirebaseFirestore.instance.collection('chatMessages').snapshots();
+    return snapshots;
+  } catch (error, stackTrace) {
+    debugPrint(error.toString());
+    debugPrintStack(stackTrace: stackTrace);
+
+    rethrow;
+  }
+});
+
+
+/*
+//? all user provider except current user
+final otherUserProvider = StreamProvider<List<ChatUserModel>>((ref) {
+  try {
+    final FirebaseAuth user = FirebaseAuth.instance;
+
+    final snapshots = FirebaseFirestore.instance
+        .collection('chatUsers')
+        .where('user_id', isNotEqualTo: user.currentUser!.uid)
+        .snapshots();
+
+    final users = snapshots.map((snap) {
+      return snap.docs
+          .map((doc) => ChatUserModel.fromJson(doc.data()))
+          .toList();
+    });
+
+    return users;
+  } on FirebaseException catch (error, stackTrace) {
+    debugPrint(error.message);
+    debugPrintStack(stackTrace: stackTrace);
+    rethrow;
+  } catch (e, stackTrace) {
+    debugPrint("Unexpected Error: $e");
+    debugPrintStack(stackTrace: stackTrace);
+    rethrow;
+  }
+});
+ */
