d99e39734770eef650a103177100b213ea5a0c84
1Shibam <hehe.shibam@gmail.com>
2025-03-20 14:41:20 +0530

    update user list with last message

--- lib/screens/home/widget/home_screen_widgets/users_list.dart
+++ lib/screens/home/widget/home_screen_widgets/users_list.dart
@@ -1,19 +1,20 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quick_chat/Exports/common_exports.dart';
 import 'package:quick_chat/model/user_model.dart';
+import 'package:quick_chat/providers/messages_provider.dart';
 import 'package:quick_chat/screens/chat/chat_screen.dart';
 import 'package:quick_chat/widgets/chat_wdgets/full_screen_image.dart';
 
-class UsersList extends StatelessWidget {
-  const UsersList({
-    super.key,
-    required this.filteredSearch,
-  });
+class UsersList extends ConsumerWidget {
+  const UsersList(
+      {super.key, required this.filteredSearch, required this.lastMessage});
 
   final List<ChatUserModel> filteredSearch;
+  final String lastMessage;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     const String emptyProfile = 'https://i.imgur.com/PcvwDlW.png';
     return ListView.builder(
       itemCount: filteredSearch.length,
@@ -48,9 +49,31 @@ class UsersList extends StatelessWidget {
             singleUser.username,
             style: AppTextStyles.heading3,
           ),
-          subtitle: Text(
-            'Lmao',
-            style: AppTextStyles.caption,
+          subtitle: Consumer(
+            builder: (context, ref, child) {
+              final lastMessage =
+                  ref.watch(lastMessageProvider(singleUser.userID));
+
+              return lastMessage.when(
+                data: (message) {
+                  if (message == null) {
+                    return Text(
+                      singleUser.bio,
+                      style: AppTextStyles.caption,
+                    );
+                  }
+
+                  return Text(
+                    message.message,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  );
+                },
+                loading: () => const CircularProgressIndicator(),
+                error: (error, stackTrace) =>
+                    const Text("Error loading message"),
+              );
+            },
           ),
         );
       },
